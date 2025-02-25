c5e24d25c534d1e3a9725d2c128b2c08c8abd677
Michel Szinavel <michel.szinavel@gmail.com>
2025-02-25 09:09:10 -0300

    Adding production env

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -6,11 +6,13 @@ bundle install
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
 npm install -g yarn
+yarn install
 yarn build:css
 
 # If you're using a Free instance type, you need to
 # perform database migrations in the build command.
 # Uncomment the following line:
 
+bundle exec rails db:create
 bundle exec rails db:migrate
 bundle exec rails db:seed
